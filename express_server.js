46c3fa34f03ec4aaf000bbb395722cc3177f2427
Ryan Zhen <rjhzhen@gmail.com>
2022-11-01 17:35:06 -0400

    short and long url linked

--- express_server.js
+++ express_server.js
@@ -22,6 +22,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
