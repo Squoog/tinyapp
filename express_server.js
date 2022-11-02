028311fa428317e8fdeaa2eddfa57030946a811b
Ryan Zhen <rjhzhen@gmail.com>
2022-11-01 20:31:37 -0400

    delete button now working

--- express_server.js
+++ express_server.js
@@ -18,6 +18,7 @@ const generateRandomString = function() {
   return randString;
 }
 
+// Home page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -33,30 +34,24 @@ app.listen(PORT, () => {
 
 app.use(express.urlencoded({ extended: true }));
 
+// Test json
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Test link
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
- 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
-
+// IMPORTANT: Keep this one before /urls/:id
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: 'http://localhost:8080/urls/:id' };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
@@ -65,7 +60,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// Adds a new url
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  console.log(req.body);
+  const randomUrl = generateRandomString();
+  urlDatabase[randomUrl] = req.body.longURL;
+  res.redirect("/urls");
 });
+
+// Delete
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+})
